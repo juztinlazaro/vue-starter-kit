1cb7873fdab5ff12f9d66cef4c99d26fe03c2b91
justin lazaro <juztinlazaro@gmail.com>
2018-11-05 17:30:09 +0800

    update code split

---
+++ src/module/Company/info.vue
@@ -0,0 +1,39 @@
+tem<template>
+  <div>
+    <h1> Info </h1>
+
+    <button @click="handleInfo">
+      give info
+    </button>
+  </div>
+</template>
+
+<script>
+import CompanyDetails from '@/common/mixin/CompanyDetails';
+
+export default {
+  props: ['getInfo'],
+  data() {
+    return {
+      info: 'yay',
+      show: true,
+      something: [
+        {
+          id: 1,
+          name: 'dsadas'
+        },
+        {
+          id: 2,
+          name: 'dsadas 2'
+        }
+      ]
+    }
+  },
+  methods: {
+    handleInfo() {
+      console.log('handleInfo');
+      this.getInfo(this.info, this.show, this.something);
+    }
+  }
+}
+</script>
